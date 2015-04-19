865347d9b55f33fad986a3a9b144af2cc00a8c04
Kenley <winnt253@hotmail.com>
2015-04-19 22:53:43 +1200

    - Fixed test_steamapi to test the async request call using tornado ioloop - Test debug logging

--- tests/test_steamapi.py
+++ tests/test_steamapi.py
@@ -1,8 +1,15 @@
+import logging
 import unittest
 import steamapi
+from tornado.testing import AsyncTestCase, gen_test
 from tornado.ioloop import IOLoop
+from tornado.httpclient import AsyncHTTPClient
 
-class EndpointsTestCase(unittest.TestCase):
+logger = logging.getLogger('dotaware')
+TEST_URL = ('http://api.steampowered.com/IDOTA2Match_570/'
+    'GetLiveLeagueGames/v1')
+
+class EndpointsTestCase(AsyncTestCase):
     '''
     Test the URL builder.
     Test the availability of APIs and do sanity checks on them
@@ -12,17 +19,29 @@ class EndpointsTestCase(unittest.TestCase):
         '''
         Requires update if the tested API ever changes
         '''
-        TEST_URL = ('http://api.steampowered.com/IDOTA2Match_570/'
-            'GetLiveLeagueGames/v1?key={}').format(steamapi.APIKEY)
-        url = steamapi.build_request('/'.join(
+        url = steamapi.build_endpoint('/'.join(
             ['IDOTA2Match_570', 'GetLiveLeagueGames', 'v1']))
         self.assertEqual(TEST_URL, url)
     
-    #TODO: need tornado IOLoop to test coroutines
-#    def test_get_live_league_games(self):
-#        data = steamapi.get_live_league_games()
-#        self.assertEqual(type(data), dict)
-#        #TODO: Test the returned response
-#
+    @gen_test
+    def test_async_request_success(self):
+        '''
+        Test it runs properly on a known endpoint
+        '''
+        #Make AsyncHTTPClient use this test's IOLoop
+        steamapi.CLIENT = AsyncHTTPClient(self.io_loop)
+        #Check that the request is logged as DEBUG
+        with self.assertLogs(logger, logging.DEBUG) as log:
+            data = yield steamapi.async_request(TEST_URL)
+        self.assertEqual(type(data), dict)
+        req = '{}?key={}'.format(TEST_URL, steamapi.APIKEY)
+        self.assertEqual(len(log.records), 1)
+        self.assertEqual(req, log.records[0].getMessage())
+
+    #def test_get_live_league_games(self):
+    #    data = steamapi.get_live_league_games()
+    #    self.assertEqual(type(data), dict)
+    #    #TODO: Test the returned response
+
 if __name__ == '__main__':
     unittest.main()
